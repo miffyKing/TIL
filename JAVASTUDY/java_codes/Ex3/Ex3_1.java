6932734bb83f6a3099a1ac437b13c7823112292a
조병화 <whqudghk28@gmail.com>
2022-10-25 13:56:24 +0900

    20221025

---
+++ JAVASTUDY/java_codes/Ex3/Ex3_1.java
@@ -0,0 +1,21 @@
+public class Ex3_1 {
+    public static void main(String[] args) {
+
+        /// ** 사칙연산, 소수점 어디에서 반올림 .. ** ///
+//        double pi = 3.141592;
+//        double shortPi = Math.round(pi*1000) / 1000.0;
+//        System.out.println(shortPi);
+
+//        Math.round(3.141592 * 1000)
+//         -> Math.round(3142.592)
+//         -> 3142
+//         3142 / 1000.0 = 3.142
+
+
+        /// ** 비교 연산자, 문자열의 비교 ** ///
+        String s1, s2;
+        s1 = "abcde";
+        s2 = "abcdE";
+        System.out.println(s1.equals(s2));
+    }
+}
