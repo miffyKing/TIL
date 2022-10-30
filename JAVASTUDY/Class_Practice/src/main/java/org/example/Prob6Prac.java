b65354076c6c79fb946e4e310d960f425937492c
miffyKing <whqudghk28@gmail.com>
2022-10-30 17:56:33 +0900

    7refactoring

--- JAVASTUDY/Class_Practice/src/main/java/org/example/Prob6Prac.java
+++ JAVASTUDY/Class_Practice/src/main/java/org/example/Prob6Prac.java
@@ -4,6 +4,114 @@
 //import java.util.HashSet;
 //import java.util.Iterator;
 //import java.util.List;
+//import java.util.regex.Pattern;
+//
+//class Form {
+//
+//  private List<List<String>> forms = new ArrayList<>();
+//  //private List <String> answer = new ArrayList<>();
+//  private HashSet<String> realAnswer = new HashSet<String>();
+//  private List<HashSet<String>> parseEachName = new ArrayList<HashSet<String>>(); // 별명별 부분문자열 set을 저장하는 배열
+//
+//  Form(List<List<String>> form) {
+//    ValidityChecker vc = new ValidityChecker(form);
+//    this.forms = form;
+//  }
+//
+//  List<String> getAnswer() {
+//    makeParseEachName();        //parseEachName 완성.
+//    findOverlap(0, parseEachName.size(), 1);
+//    findOverlap(parseEachName.size() - 1, -1, -1);     //이거 음수로 넘어가도 괜찮은지 꼭 확인. -> 두글자로 안잘리는 닉네임?
+//    System.out.println(realAnswer);
+//    return new ArrayList<>(realAnswer);
+//  }
+//
+//  void makeParseEachName() {
+//    for (int i = 0; i < forms.size(); i++) {
+//      List<String> chunk = forms.get(i);
+//      //System.out.println(chunk);
+//      HashSet<String> NameSubstring = getAllTwoWords(chunk.get(1));
+//      parseEachName.add(NameSubstring);
+//    }
+//  }
+//
+//  HashSet<String> getAllTwoWords(String s) {
+//    HashSet<String> subStrings = new HashSet<String>();
+//    for (int i = 0; i < s.length() - 1; i++) {
+//      String parsedS = s.substring(i, i + 2);
+//      subStrings.add(parsedS);
+//    }
+//    return subStrings;
+//  }
+//
+//  void findOverlap(int from, int to, int iterBy) {
+//    HashSet<String> parsedTotalSetDown = new HashSet<String>();
+//    for (int i = from; i != to; i = i + iterBy) {        // 위에서 아래로 내려가는 과정
+//      HashSet<String> hashEachName = new HashSet<String>();   // parseEachName에서 하나하나의 set
+//      hashEachName = parseEachName.get(i);
+//      Iterator<String> it = hashEachName.iterator();
+//      for (String iter : hashEachName) {
+//        if (parsedTotalSetDown.contains(iter) == true) { // 있다는 뜻은, 저 i 인덱스의 메일이 중복된 새2끼란 뜻
+//          realAnswer.add(forms.get(i).get(0));            // . 두개 쓴거 없애야함.
+//        }
+//        parsedTotalSetDown.add(iter);
+//      }
+//    }
+//  }
+//}
+//
+//class ValidityChecker {
+//
+//  ValidityChecker(List<List<String>> input) {
+//    emailValidityCheck(input);
+//    nicknameValidityCheck(input);
+//    wordLengthValidityCheck(input);
+//  }
+//  private void emailValidityCheck(List<List<String>> input) {
+//    //@뒷부분에 email.com 만 있는가.
+//    //@앞부분이 숫자, 영어 소문자만 있는가
+//    for (List<String> strings : input) {
+//      String[] emailFront = strings.get(0).split("@");
+//      if (emailFront.length != 2) {   //@가 2개이상일때
+//        throw new IllegalArgumentException("이메일 양식 @@두개 를 만족하지 않습니다.");
+//      }
+//      if (!Pattern.matches("^[a-z0-9A-Z._-]*", emailFront[0])) {    //@앞이 영어,숫자등이 아닐때
+//        throw new IllegalArgumentException("이메일 양식을 만족하지 않습니다.");
+//      }
+//      if (!emailFront[1].equals("email.com")) { //@뒤가 email.com이 아닐때
+//        throw new IllegalArgumentException("이메일 양식을 만족하지 않습니다.");
+//      }
+//    }
+//  }
+//  private void nicknameValidityCheck(List<List<String>> input) {
+//    //닉네임은 한글만으로 되어있는가
+//    for (List<String> strings : input) {
+//      String Nickname = strings.get(1);
+//      if (!Pattern.matches("[가-힣]*$", Nickname)) {
+//        throw new IllegalArgumentException("닉네임이 한글이 아닙니다.");
+//      }
+//    }
+//  }
+//  private void wordLengthValidityCheck(List<List<String>> input) {
+//    //닉네임 길이가 1~20인가
+//    //이메일 11~20 미만
+//    //크루가 1~10000 이하인가?
+//    if (input.size() < 1 || input.size() > 10000) {
+//      throw new IllegalArgumentException("인원 수가 조건 범위 내에 있지 않습니다.");
+//    }
+//    for (List<String> strings : input) {
+//      String Nickname = strings.get(1);
+//      String Email = strings.get(0);
+//      if (Nickname.length() < 1 || Nickname.length() >= 20) {
+//        throw new IllegalArgumentException("닉네임의 길이가 조건 범위 내에 있지 않습니다.");
+//      }
+//      if (Email.length() < 11 || Email.length() >= 20) {
+//        throw new IllegalArgumentException("이메일의 길이가 조건 범위 내에 있지 않습니다.");
+//      }
+//    }
+//  }
+//
+//}
 //
 //public class Prob6Prac {
 //
@@ -15,62 +123,63 @@
 //        List.of("mj@email.com", "엠제이"),
 //        List.of("nowm@email.com", "이제엠")
 //    );
-//    List<String> answer = List.of("answer");
-//    HashSet<String> realAnswer = new HashSet<String>();
+//    Form form = new Form(forms);
+//    System.out.println(form.getAnswer());
+//    //return (form.getAnswer());
+////
+////    HashSet<String> realAnswer = new HashSet<String>();
+////    HashSet<String> parsedTotalSetDown = new HashSet<String>(); //모든 부분문자열을 넣을, 아래방향으로 진행시킬 set
+////    HashSet<String> parsedTotalSetUp = new HashSet<String>(); //모든 부분문자열을 넣을, 위으로 진행시킬 set
+////    List<HashSet<String>> parseEachName = new ArrayList<HashSet<String>>(); // 별명별 부분문자열 set을 저장하는 배열
+////
+////    for (int i = 0; i < forms.size(); i++) {
+////      //부분문자열 만드는거 어케 할까?
+////      HashSet<String> NameSubstring = new HashSet<String>();
+////      NameSubstring = getSubstring(forms.get(i).get(1));      // {"이메일", "별명"} 중 별명
+////      parseEachName.add(NameSubstring);
+////    }           // 부분문자열을 ParseEachName 배열에 저장하는데 성공함.
+////    for (int i = 0; i < parseEachName.size(); i++) {        // 위에서 아래로 내려가는 과정
+////      HashSet<String> hashEachName = new HashSet<String>();   // parseEachName에서 하나하나의 set
+////      hashEachName = parseEachName.get(i);
+////      Iterator<String> it = hashEachName.iterator();
+////      //int index = 0;
+////      for (String iter : hashEachName) {
+////        if (parsedTotalSetDown.contains(iter) == true) { // 있다는 뜻은, 저 i 인덱스의 메일이 중복된 새2끼란 뜻
+////          realAnswer.add(forms.get(i).get(0));            // . 두개 쓴거 없애야함.
+////        }
+////        parsedTotalSetDown.add(iter);
+////      }
+////    }
+////    for (int i = parseEachName.size() - 1; i >= 0; i--) {     // 밑에서 위로 올라가는 과정.
+////      HashSet<String> hashEachName = new HashSet<String>();
+////      hashEachName = parseEachName.get(i);
+////      Iterator<String> it = hashEachName.iterator();
+////      //int index = 0;
+////      for (String iter : hashEachName) {
+////        if (parsedTotalSetUp.contains(iter) == true) { // 있다는 뜻은, 저 i 인덱스의 메일이 중복된 새2끼란 뜻
+////          realAnswer.add(forms.get(i).get(0));            // . 두개 쓴거 없애야함.
+////        }
+////        parsedTotalSetUp.add(iter);
+////      }
+////    }
+////    answer = new ArrayList<>(realAnswer);
+////    // System.out.println(answer);
+////    //set 배열들에 별명들을 다 부분문자열로 나누어 저장한다
+////    //위에서부터 아래로 내려가며 큰 set 에 넣어간다. 이미 set에 있는 경우 중복이기 때문에 현재 set에 집어넣고 있는 아이의 cnt를 증가시킨다.
+////    // 아니면 리스트에 바로 이메일을 입력을 한다.
+////    //밑에서부터 위로 올라가며 다시 set에 넣는다. 이미 set에 있는 경우 중복이기 때문에 현재 set에 집어넣고 있는 아이의 이메일을 리스트에 넣는다.
+////  }
 //
-//    HashSet<String> parsedTotalSetDown = new HashSet<String>(); //모든 부분문자열을 넣을, 아래방향으로 진행시킬 set
-//    HashSet<String> parsedTotalSetUp = new HashSet<String>(); //모든 부분문자열을 넣을, 위으로 진행시킬 set
-//    List<HashSet<String>> parseEachName = new ArrayList<HashSet<String>>(); // 별명별 부분문자열 set을 저장하는 배열
-//
-//    for (int i = 0; i < forms.size(); i++) {
-//      //부분문자열 만드는거 어케 할까?
-//      HashSet<String> NameSubstring = new HashSet<String>();
-//      NameSubstring = getSubstring(forms.get(i).get(1));      // {"이메일", "별명"} 중 별명
-//      parseEachName.add(NameSubstring);
-//    }           // 부분문자열을 ParseEachName 배열에 저장하는데 성공함.
-//    for (int i = 0; i < parseEachName.size(); i++) {        // 위에서 아래로 내려가는 과정
-//      HashSet<String> hashEachName = new HashSet<String>();   // parseEachName에서 하나하나의 set
-//      hashEachName = parseEachName.get(i);
-//      Iterator<String> it = hashEachName.iterator();
-//      //int index = 0;
-//      for (String iter : hashEachName) {
-//        if (parsedTotalSetDown.contains(iter) == true) { // 있다는 뜻은, 저 i 인덱스의 메일이 중복된 새2끼란 뜻
-//          realAnswer.add(forms.get(i).get(0));            // . 두개 쓴거 없애야함.
-//        }
-//        parsedTotalSetDown.add(iter);
-//      }
-//    }
-//    for (int i = parseEachName.size() - 1; i >= 0; i--) {     // 밑에서 위로 올라가는 과정.
-//      HashSet<String> hashEachName = new HashSet<String>();
-//      hashEachName = parseEachName.get(i);
-//      Iterator<String> it = hashEachName.iterator();
-//      //int index = 0;
-//      for (String iter : hashEachName) {
-//        if (parsedTotalSetUp.contains(iter) == true) { // 있다는 뜻은, 저 i 인덱스의 메일이 중복된 새2끼란 뜻
-//          realAnswer.add(forms.get(i).get(0));            // . 두개 쓴거 없애야함.
-//        }
-//        parsedTotalSetUp.add(iter);
-//      }
-//    }
-//    answer = new ArrayList<>(realAnswer);
-//    // System.out.println(answer);
-//    //set 배열들에 별명들을 다 부분문자열로 나누어 저장한다
-//    //위에서부터 아래로 내려가며 큰 set 에 넣어간다. 이미 set에 있는 경우 중복이기 때문에 현재 set에 집어넣고 있는 아이의 cnt를 증가시킨다.
-//    // 아니면 리스트에 바로 이메일을 입력을 한다.
-//    //밑에서부터 위로 올라가며 다시 set에 넣는다. 이미 set에 있는 경우 중복이기 때문에 현재 set에 집어넣고 있는 아이의 이메일을 리스트에 넣는다.
-//
+////  public static HashSet<String> getSubstring(String s) {
+////    HashSet<String> subStrings = new HashSet<String>();
+////    for (int i = 2; i <= s.length(); i++) {
+////      for (int j = 0; j <= s.length() - i; j++) {
+////        String parsedS = s.substring(j, j + i);
+////        //  System.out.println(parsedS);
+////        subStrings.add(parsedS);
+////      }
+////    }
+////    return subStrings;
+////  }
 //  }
-//
-//  public static HashSet<String> getSubstring(String s) {
-//    HashSet<String> subStrings = new HashSet<String>();
-//    for (int i = 2; i <= s.length(); i++) {
-//      for (int j = 0; j <= s.length() - i; j++) {
-//        String parsedS = s.substring(j, j + i);
-//        //  System.out.println(parsedS);
-//        subStrings.add(parsedS);
-//      }
-//    }
-//    return subStrings;
-//  }
-//
 //}
--- JAVASTUDY/Class_Practice/src/main/java/org/example/Prob7Prac.java
+++ JAVASTUDY/Class_Practice/src/main/java/org/example/Prob7Prac.java
@@ -3,11 +3,151 @@ package org.example;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map.Entry;
+import java.util.regex.Pattern;
 
+class Friend {
+
+  String user;
+  List<List<String>> friends = new ArrayList<>();
+  List<String> visitors = new ArrayList<>();
+  private HashMap<String, HashSet<String>> relationTable = new HashMap<>();
+  private HashMap<String, Integer> scoreTable = new HashMap<>();
+  Friend(String user, List<List<String>> friends , List<String> visitors) {
+    ValidityChecker vc = new ValidityChecker(user, friends, visitors);
+    RelationMaker rm = new RelationMaker(friends, visitors);
+    this.friends = friends;
+    this.visitors = visitors;
+    this.user = user;
+    relationTable = rm.makeRelationTable();
+    scoreTable = makeScoreTable();
+  }
+  private HashMap<String, Integer> makeScoreTable() {
+    //scoretable 생성
+    for (int i = 0; i < friends.size(); i++) {
+      scoreTable.put(friends.get(i).get(0), 0);
+      scoreTable.put(friends.get(i).get(1), 0);
+    }
+    for (int i = 0; i < visitors.size(); i++) {
+      scoreTable.put(visitors.get(i), 0);
+    }
+    return scoreTable;
+  }
+  List<String> getAnswer() {
+    ScoreCalculator sc = new ScoreCalculator(user,visitors, relationTable, scoreTable);
+    return sc.calculateScore();
+  }
+
+}
+
+class RelationMaker {
+  private List<List<String>> friends;
+  private List<String> visitors;
+  RelationMaker(List<List<String>> friends, List<String> visitors) {
+    this.friends = friends;
+    this.visitors = visitors;
+  }
+  HashMap<String, HashSet<String>> makeRelationTable() {
+    HashMap<String, HashSet<String>> relationTable = new HashMap<>();
+    for (int i = 0; i < friends.size(); i++) {
+      // 만약에 key가 없을 경우
+
+      if (!relationTable.containsKey(friends.get(i).get(0))) {   // 도넛이 관계테이블의 키로 없을 떄
+        HashSet<String> nameSet = new HashSet<>();
+        nameSet.add(friends.get(i).get(1));
+        relationTable.put(friends.get(i).get(0), nameSet);
+      }
+      if (!relationTable.containsKey(friends.get(i).get(1))) {   // 도넛이 관계테이블의 키로 없을 떄
+        HashSet<String> nameSet = new HashSet<>();         //map에서 Value로 들어갈 set
+        nameSet.add(friends.get(i).get(0));                 //set에 친구 박고 map에 넣기
+        relationTable.put(friends.get(i).get(1), nameSet);
+      }
+      // 이미 관계테이블에 이미 key가 있는 경우.
+      relationTable.get(friends.get(i).get(0)).add(friends.get(i).get(1));
+      relationTable.get(friends.get(i).get(1)).add(friends.get(i).get(0));
+      ////최종적으로 관계 테이블 완성 (1.)
+      /// 전체 이름과 점수를 담을 SCoreTable 에 이름들을 다 집어넣는다.
+    }
+    return relationTable;
+  }
+}
+
+class ValidityChecker {
+  ValidityChecker(String user, List<List<String>> friends, List<String> visitors) {
+    userValidityCheck(user);
+    friendsValidityCheck(friends);
+    visitorsValidityCheck(visitors);
+  }
+  private void userValidityCheck(String user) {
+  }
+  private void friendsValidityCheck(List<List<String>> friends) {
+  }
+  private void visitorsValidityCheck(List<String> visitors) {
+  }
+}
+
+class ScoreCalculator {
+  String user;
+  private HashMap<String, HashSet<String>> relationTable = new HashMap<>();
+  private HashMap<String, Integer> scoreTable = new HashMap<>();
+  List<String> visitors;
+  ScoreCalculator (String user, List<String> visitors ,HashMap<String, HashSet<String>> relationTable, HashMap<String, Integer> scoreTable) {
+    this.visitors = visitors;
+    this.relationTable = relationTable;
+    this.scoreTable = scoreTable;
+    this.user = user;
+  }
+
+  List<String> calculateScore() {
+    List<String> answer = new ArrayList<>();
+    for (int i = 0; i < visitors.size(); i++) {
+      if (scoreTable.containsKey(visitors.get(i))) {
+        int score = scoreTable.get(visitors.get(i));
+        score++;
+        scoreTable.put(visitors.get(i), score);
+      }
+      if (!scoreTable.containsKey(visitors.get(i))) {
+        scoreTable.put(visitors.get(i), 1);
+      }
+    }
+    for (String iter : relationTable.get(user)) {
+      HashSet<String> userFriendsFriend = new HashSet<>();
+      userFriendsFriend = relationTable.get(iter);        //마르코의 친구 도넛의 친구 리스트
+      for (String iter2 : userFriendsFriend) {
+        int score = scoreTable.get(iter2);
+        scoreTable.put(iter2, score + 10);
+      }
+    }
+    scoreTable.remove(user);
+    for (String iter : relationTable.get(user)) {
+      scoreTable.remove(iter);
+    }
+    List<Entry<String, Integer>> list_entries = new ArrayList<Entry<String, Integer>>(
+        scoreTable.entrySet());
+    Collections.sort(list_entries, new Comparator<Entry<String, Integer>>() {
+      // compare로 값을 비교
+      public int compare(Entry<String, Integer> obj1, Entry<String, Integer> obj2) {
+        // 내림 차순 정렬
+        int res = obj2.getValue() - obj1.getValue();
+        if (res == 0) {
+          obj1.getKey().compareTo(obj2.getKey());
+        }
+        return res;
+      }
+    });
+    for (int i = 0; i < list_entries.size(); i++) {
+      answer.add(list_entries.get(i).getKey());
+      if (i == 4) {
+        break;
+      }
+    }
+    return answer;
+  }
+}
 public class Prob7Prac {
-
   public static void main(String[] args) {
     String user = "mrko";
     List<List<String>> friends = List.of(
@@ -19,69 +159,87 @@ public class Prob7Prac {
         List.of("shakevan", "mrko")
     );
     List<String> visitors = List.of("bedi", "bedi", "donut", "bedi", "shakevan");
-    List<Pair> pairList = new ArrayList<>();
-    HashSet<String> userFriendList = new HashSet<>();
-    HashSet<Pair<String, Integer>> allNameList = new HashSet<>();
+    List<String> answer = new ArrayList<>();
 
-    for (String iter : visitors) {        //5. 찐따들 전체이름set에 추가.
-      if (!allNameList.contains(iter)) {
-        allNameList.add(new Pair(iter,1));    //폭발 -> SET이라 바로 FInd가 안됨.
-      }
-
-
-
-    }
-    for (List<String> iter : friends) {
-      pairList.add(new Pair(iter.get(0), iter.get(1)));
-      pairList.add(new Pair(iter.get(1), iter.get(0)));       //1. 리스트에 순서 상관없이 다 때려박음.
-      if (user.equals(iter.get(0)) ||user.equals(iter.get(1))) {  //3. 마르코가 들어간 pair가 나오면 마르코 친구 set에 넣는다.
-        if (user.equals(iter.get(0))) {
-          userFriendList.add(iter.get(1));
-        }
-        if (user.equals(iter.get(1))) {
-          userFriendList.add(iter.get(0));
-        }
-      }
-
-      if (!allNameList.contains(iter.get(0))) {           //2. 이름들로 구성된 이름 전부 든 set 만들기 (나중에 찐따들 추가), allNameList
-        allNameList.add(new Pair(iter.get(0), 0));
-      }
-      if (!allNameList.contains(iter.get(1))) {
-        allNameList.add(new Pair(iter.get(1), 0));
-      }
-    }
-//    System.out.println(pairList.size());
-//    System.out.println(pairList.get(0).getX());
-    Object
-    pairList.sort(new Comparator<>() {
-      @Override
-      public int compare(Pair o1, Pair o2) {
-        return 0;
-      }
-    });
-
-
-    for (Pair iter : pairList) {
-      System.out.println(iter.getX() + " " + iter.getY());
-    }
-
-  }
-}
-
-class Pair<String, T> {
-
-  private String x;
-  private T y;
-
-  public Pair(String x, T y) {
-    this.x = x;
-    this.y = y;
-  }
-
-  public String getX() {
-    return x;
-  }
-  public T getY() {
-    return y;
+    Friend friend = new Friend(user, friends, visitors);
+    System.out.println(friend.getAnswer());
+//
+//    HashMap<String, HashSet<String>> relationTable = new HashMap<>(); //도넛 - {안돌,준, 마코} 들 저장.
+//    HashMap<String, Integer> scoreTable = new HashMap<>();     // {도넛 - 20점} 저장
+//    for (int i = 0; i < friends.size(); i++) {
+//      // 만약에 key가 없을 경우
+//      if (relationTable.containsKey(friends.get(i).get(0)) != true) {   // 도넛이 관계테이블의 키로 없을 떄
+//        HashSet<String> nameSet = new HashSet<>();
+//        nameSet.add(friends.get(i).get(1));
+//        relationTable.put(friends.get(i).get(0), nameSet);
+//      }
+//      if (relationTable.containsKey(friends.get(i).get(1)) != true) {   // 안돌이 관계테이블의 키로 없을 떄
+//        HashSet<String> nameSet = new HashSet<>();         //map에서 Value로 들어갈 set
+//        nameSet.add(friends.get(i).get(0));                 //set에 친구 박고 map에 넣기
+//        relationTable.put(friends.get(i).get(1), nameSet);
+//      }
+//      // 이미 관계테이블에 이미 key가 있는 경우.
+//      relationTable.get(friends.get(i).get(0)).add(friends.get(i).get(1));
+//      relationTable.get(friends.get(i).get(1)).add(friends.get(i).get(0));
+//      ////최종적으로 관계 테이블 완성 (1.)
+//      scoreTable.put(friends.get(i).get(0), 0);
+//      scoreTable.put(friends.get(i).get(1), 0);
+//      /// 전체 이름과 점수를 담을 SCoreTable 에 이름들을 다 집어넣는다.
+//    }
+//
+//    for (int i = 0; i < visitors.size(); i++) {
+//      if (scoreTable.containsKey(visitors.get(i)) == true) {
+//        int score = scoreTable.get(visitors.get(i));
+//        score++;
+//        scoreTable.put(visitors.get(i), score);
+//      }
+//      if (scoreTable.containsKey(visitors.get(i)) == false) {
+//        scoreTable.put(visitors.get(i), 1);
+//      }
+//    }                 //visitor 들 순회하며 찐따들 넣어주고, 이미 있는 이름이면 1 증가.
+//
+//    //4. user의 친구이름들을 1.에서 만든 map에서 찾아 그 value들의 친구들의 점수를 증가시켜준다.
+//
+//    for (String iter : relationTable.get(user)) {
+//      HashSet<String> userFriendsFriend = new HashSet<>();
+//      userFriendsFriend = relationTable.get(iter);        //마르코의 친구 도넛의 친구 리스트
+//      for (String iter2 : userFriendsFriend) {
+//        int score = scoreTable.get(iter2);
+//        scoreTable.put(iter2, score + 10);
+//      }
+//    }
+//
+//    //5.  scoreTable 의 값을 확인하고, user과 이미 USER의 친구들을 제외한다.
+//    scoreTable.remove(user);
+//    for (String iter : relationTable.get(user)) {
+//      scoreTable.remove(iter);
+//    }
+//
+//    //6. 정렬
+//    // Map.Entry 리스트 작성
+//    List<Entry<String, Integer>> list_entries = new ArrayList<Entry<String, Integer>>(
+//        scoreTable.entrySet());
+//
+//    // 비교함수 Comparator를 사용하여 오름차순으로 정렬
+//    Collections.sort(list_entries, new Comparator<Entry<String, Integer>>() {
+//      // compare로 값을 비교
+//      public int compare(Entry<String, Integer> obj1, Entry<String, Integer> obj2) {
+//        // 내림 차순 정렬
+//        int res = obj2.getValue() - obj1.getValue();
+//        if (res == 0) {
+//          obj1.getKey().compareTo(obj2.getKey());
+//        }
+//        return res;
+//      }
+//    });
+//
+//    for (int i = 0; i < list_entries.size(); i++) {
+//      answer.add(list_entries.get(i).getKey());
+//      if (i == 4) {
+//        break;
+//      }
+//    }
+//    System.out.println(answer);
+//    //return answer;
   }
 }
